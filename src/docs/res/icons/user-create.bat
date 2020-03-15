28cc9e1ab7c4b2b5754353d5672638202695db17
Vojto Bálint <38936748+vojtob@users.noreply.github.com>
2020-03-15 07:30:37 +0100

    remove icons

--- src/docs/res/icons/user-create.bat
+++
@@ -1,2 +0,0 @@
-magick -gravity NorthEast user-empty.png ( communities.png -resize 600x600 ) -composite user-cpp.png
-magick -gravity NorthEast user-empty.png ( badgeOK.png -resize 600x600 ) -composite user-external.png
