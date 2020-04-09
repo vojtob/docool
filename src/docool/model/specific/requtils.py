904feaad3af89f80b2612956a0daf01f8b62fda6
Vojto Bálint <38936748+vojtob@users.noreply.github.com>
2020-04-09 08:05:04 +0200

    minor

--- src/docool/model/specific/requtils.py
+++ src/docool/model/specific/requtils.py
@@ -13,15 +13,17 @@ def generate_requirements(chapter, subchapter, reqcount):
         requirements.append(reqline_format.format(reqID=reqID, reqname=reqname))
     return requirements
 
+def generate_chapter(chapter, listofsizes):
+    r = []
+    for i,s in enumerate(listofsizes):
+        r.extend(generate_requirements(chapter, 1+i, s))
+    return r
+
 if __name__ == "__main__":
     outfile_elements_name = 'C:/Projects_src/Work/temp/reqelements.csv'
-    r = []
-    requirements9 = generate_requirements(9, 1, 1)
-    r.extend(requirements9)
-    # requirements10 = generate_requirements(10, 1, 8)
-    # r.extend(requirements10)
-    # requirements11 = generate_requirements(11, 1, 6)
-    # r.extend(requirements11)
+    r = generate_chapter(8, [8, 12, 13, 1])
+    # requirements9 = generate_requirements(9, 1, 1)
+    # r.extend(requirements9)
     with open(outfile_elements_name, mode="w", encoding='utf-8') as outfile_elements:
         outfile_elements.writelines(r)
     print('DONE: requirements file {0} generated'.format(outfile_elements_name))
